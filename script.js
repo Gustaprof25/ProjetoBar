ab2e0226da39b2463e55f06d9fef2400b7c1ab6c
Gustaprof25 <66458779+Gustaprof25@users.noreply.github.com>
2021-02-03 16:24:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 		function calc(){
-			if (document.querySelector("input[type=number]").value == "") {
+			for (var i = 0; i <= 32; i++) {
+				if (document.querySelector("input[type=number]").value == "") {
 					var valorItem = 0;
 				}else{
-				var qnt = document.querySelector("input[type=number]").value;
-				var mult = parseInt(document.querySelector("input[name=valorbtt]").value);
-				var valorItem = qnt * mult
-
+					var qnt = document.querySelector("input[type=number]").value;
+					var mult = parseInt(document.querySelector("input[name=valorbtt]").value);
+					var valorItem = qnt * mult
+				}	
 			}
-			
 		}
