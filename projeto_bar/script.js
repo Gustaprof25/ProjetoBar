6485a8f80ddd9182e1780bcd28e2ee8a97e409fd
Gustaprof25 <66458779+Gustaprof25@users.noreply.github.com>
2021-02-03 10:45:01 -0300

    Delete projeto_bar directory

--- projeto_bar/script.js
+++
@@ -1,7 +0,0 @@
-		function calc(){
-
-			var qnt = document.getElementByID('btt');
-			alert("O valor de btt é:" + qnt);
-			valor = qnt * 20;
-			document.getElementByID('qualquer_').value = valor;
-		}
