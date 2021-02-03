e6265cd5a5fa608c7efd3246a51f45905d84ca6f
Gustaprof25 <66458779+Gustaprof25@users.noreply.github.com>
2021-02-03 16:48:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 					var valorItem = 0;
 				}else{
 					var qnt = document.querySelector("input[type=number]").value;
-					var mult = parseInt(document.querySelector("input[name=valorbtt]").value);
+					var mult = parseInt(document.querySelector("td[class=valorbtt]").value);
 					var valorItem = qnt * mult
 				}	
 			
