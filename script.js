49ac7bedf6534950c8dbf016a2010b9e43b56482
Gustaprof25 <66458779+Gustaprof25@users.noreply.github.com>
2021-02-03 10:43:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+		function calc(){
+
+			var qnt = document.getElementByID('btt');
+			alert("O valor de btt é:" + qnt);
+			valor = qnt * 20;
+			document.getElementByID('qualquer_').value = valor;
+		}
